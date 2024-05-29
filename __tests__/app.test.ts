0957a9331dc3196fb0aceefff9b4e665c054f87c
maximlishchenko <makslis2000@gmail.com>
2024-05-29 21:33:01 +0300

    add tests for POST /deals

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -6,9 +6,10 @@ dotenv.config();
 
 
 describe("GET /deals", () => {
-
     it("should get all the deals successfully", async () => {
-        const response =  await request(app).get('/api/deals');
+        const response =  await request(app)
+            .get('/api/deals');
+
         expect(response.body.success).toBe(true);
         expect(response.statusCode).toBe(200);
         expect(response.body.data).toBeInstanceOf(Array);
@@ -17,10 +18,57 @@ describe("GET /deals", () => {
     it("should fail accessing deals without a valid api token", async () => {
         const originalApiToken = process.env.API_TOKEN;
         process.env.API_TOKEN = '';
-        const response =  await request(app).get('/api/deals');
+        const response =  await request(app)
+        .get('/api/deals');
+
         expect(response.statusCode).toBe(401);
         expect(response.body.data?.success).toBe(false);
         process.env.API_TOKEN = originalApiToken;
     });
+});
 
+
+describe("POST /deals", () => {
+    it("should create a new deal successfully", async () => {
+        const dealData = {
+            title: "myDeal"
+        };
+
+        const response = await request(app)
+            .post('/api/deals')
+            .send(dealData);
+
+        expect(response.status).toBe(201);
+        expect(response.body.success).toBe(true);
+        // expect(response.body.data.title).toBe(dealData.title);
+        expect(response.body.data).toMatchObject({
+            id: expect.any(Number),
+            title: "myDeal"
+        });
+    });
+
+    it("should fail creating a new deal if title is not provided", async () => {
+        const response = await request(app)
+            .post('/api/deals');
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body.data?.success).toBe(false);
+    });
+
+    it("should fail creating a new deal without a valid api token", async () => {
+        const originalApiToken = process.env.API_TOKEN;
+        process.env.API_TOKEN = '';
+
+        const dealData = {
+            title: "myDeal"
+        };
+
+        const response =  await request(app)
+            .post('/api/deals')
+            .send(dealData);
+
+        expect(response.statusCode).toBe(401);
+        expect(response.body.data?.success).toBe(false);
+        process.env.API_TOKEN = originalApiToken;
+    });
 });
